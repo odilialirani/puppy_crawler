31a24b8d4a5a9a2d2a815cdca314ae80e703e084
odilialirani <oclirani@gmail.com>
2020-05-13 22:01:39 -0500

    crawl one pager

--- centipede/centipede/spiders/puppo.py
+++ centipede/centipede/spiders/puppo.py
@@ -1,11 +1,34 @@
-# -*- coding: utf-8 -*-
+# from scrapy.spiders import CrawlSpider, Rule
+# from scrapy.linkextractors import LinkExtractor
+
+
+# class PuppoSpider(CrawlSpider):
+#     name = 'puppo'
+    # allowed_domains = ['www.ilshorthairrescue.com', 'www.moonsongmals.org']
+    # start_urls = [
+    #   "https://www.ilshorthairrescue.com/adoptable-dogs/",
+    #   "https://www.moonsongmals.org/adoption/mmrfosters.html"
+    # ]
+
+#     rules = (
+#         Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
+#              callback="parse_item",
+#              follow=True),)
+
+#     def parse_item(self, response):
+#         print('Processing..' + response.url)
+
+
 import scrapy
 
 
 class PuppoSpider(scrapy.Spider):
     name = 'puppo'
-    allowed_domains = ['www.puppyfind.com']
-    start_urls = ['http://www.puppyfind.com/']
+    allowed_domains = ['www.ilshorthairrescue.com', 'www.moonsongmals.org']
+    start_urls = [
+      "https://www.ilshorthairrescue.com/adoptable-dogs/",
+      "https://www.moonsongmals.org/adoption/mmrfosters.html"
+    ]
 
     def parse(self, response):
-        pass
+      print('Processing...' + response.url)
